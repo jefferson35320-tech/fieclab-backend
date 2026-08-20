110890ea2e5f117dc7854f9f5a9832a8c5b94b90
JEFFERSON LUIZ FERRAZINI JUNIOR <35320@fiecalunos.lan>
2026-08-20 20:12:24 -0300

    fix: corrige nomes dos campos da entidade Product

--- src/main/java/br/edu/fiec/FiecLab/Features/Produts/ProdutsModel/Entities/Product.java
+++ src/main/java/br/edu/fiec/FiecLab/Features/Produts/ProdutsModel/Entities/Product.java
@@ -21,18 +21,24 @@ public class Product {
     private UUID id;
 
     @Column(nullable = false)
-    private String nome;
+    private String name;
 
     @Column(nullable = false)
-    private Double preco;
+    private Double price;
 
-    private String descricao;
+    private String description;
 
-    private String imagem_em_texto;
+    private String imageUrl;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private ProductType tipo_de_Produto;
+    private ProductType type;
 
-    private Integer estoque;
+    private Integer batch;
+
+    private LocalDate mfgDate;
+
+    private LocalDate expDate;
+
+    private Integer supplierId;
 }
